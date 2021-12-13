49cabc602467702a6b41be775d82143a60c6865b
Andy Barrett <apbarret@nsidc.org>
2021-12-13 14:29:11 -0700

    initial commit of filepath

---
+++ canadian_extreme_precip/filepath.py
@@ -0,0 +1,9 @@
+'''Contains filepaths for data etc'''
+
+from pathlib import PATH
+
+HOME = Path.home()
+
+DATAPATH = HOME / 'Data' / 'Rain_on_snow'
+
+RAW_STATION_PATH = DATAPATH / 'Canada_extreme_precip' / 'Raw_station_files'
