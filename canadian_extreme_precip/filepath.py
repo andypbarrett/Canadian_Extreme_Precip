55ed751acc6a660fdfe4767fbe22af1a0a5488e1
Andy Barrett <apbarret@nsidc.org>
2021-12-14 18:10:14 -0700

    add generation of raw station filepath

--- canadian_extreme_precip/filepath.py
+++ canadian_extreme_precip/filepath.py
@@ -7,3 +7,12 @@ HOME = Path.home()
 DATAPATH = HOME / 'Data' / 'Rain_on_snow'
 
 RAW_STATION_PATH = DATAPATH / 'Canada_extreme_precip' / 'Raw_station_files'
+
+
+def raw_station_filepath(climate_identifier):
+    '''Returns filepath for station file
+
+    :climate_identifier: numeric station id
+
+    :returns: filepath pathlib Path object'''
+    return RAW_STATION_PATH / f'{climate_identifier}_climate_daily.csv'
