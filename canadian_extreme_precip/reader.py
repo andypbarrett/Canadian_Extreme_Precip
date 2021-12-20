e015814e58805a396c5740fff38f69517bd5eddf
Andy Barrett <apbarret@nsidc.org>
2021-12-20 12:39:55 -0700

    add flags and modify reader to recognize flag columns

--- canadian_extreme_precip/reader.py
+++ canadian_extreme_precip/reader.py
@@ -3,7 +3,27 @@
 import pandas as pd
 
 
+column_dtype = {
+    'MEAN_TEMPERATURE_FLAG': str,
+    'MIN_TEMPERATURE_FLAG': str,
+    'MAX_TEMPERATURE_FLAG': str,
+    'TOTAL_PRECIPITATION_FLAG': str,
+    'TOTAL_RAIN_FLAG': str,
+    'TOTAL_SNOW_FLAG': str,
+    'SNOW_ON_GROUND_FLAG': str,
+    'DIRECTION_MAX_GUST_FLAG': str,
+    'SPEED_MAX_GUST_FLAG': str,
+    'COOLING_DEGREE_DAYS_FLAG': str,
+    'HEATING_DEGREE_DAYS_FLAG': str,
+    'MIN_REL_HUMIDITY_FLAG': str,
+    'MAX_REL_HUMIDITY_FLAG': str,
+    }
+
+
 def read_station_file(station_file):
-    '''Reads raw station file'''
-    df = pd.read_csv(station_file, index_col='LOCAL_DATE', parse_dates=True)
+    '''Reads raw station file
+    A description of Flags is here
+    https://climate.weather.gc.ca/doc/Technical_Documentation.pdf'''
+    df = pd.read_csv(station_file, index_col='LOCAL_DATE',
+                     dtype=column_dtype, parse_dates=True)
     return df
