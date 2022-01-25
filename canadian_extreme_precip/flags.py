a8b761e78436fc304141caeef02cedf23ceaf3dd
Andy Barrett <apbarret@nsidc.org>
2022-01-25 16:56:21 -0700

    create flag dictionary

---
+++ canadian_extreme_precip/flags.py
@@ -0,0 +1,17 @@
+ECCC_FLAGS = {
+    '':	'Indicates an unobserved value',
+    '†': 'Data that is not subject to review by the National Climate Archives',
+    '^': 'The value displayed is based on incomplete data',
+    'A': 'Accumulated',
+    'B': 'More than one occurrence and estimated',
+    'C': 'Precipitation occurred, amount uncertain',
+    'E': 'Estimated',
+    'F': 'Accumulated and estimated',
+    'L': 'Precipitation may or may not have occurred',
+    'M': 'Missing',
+    'N': 'Temperature missing but known to be > 0'
+    'S': 'More than one occurrence',
+    'T': 'Trace',
+    'Y': 'Temperature missing but known to be < 0',
+    'NA': 'Not Available'
+    }
