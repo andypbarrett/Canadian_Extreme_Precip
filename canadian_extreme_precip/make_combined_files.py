14b8817f0e680bd7beb6c3813a8a3bbd5c1768f8
Andy Barrett <apbarret@nsidc.org>
2022-02-03 12:12:13 -0700

    fix conflict between ' and "

--- canadian_extreme_precip/make_combined_files.py
+++ canadian_extreme_precip/make_combined_files.py
@@ -169,12 +169,12 @@ def make_png_filename(location, outdir='.'):
     return this_path / f'{loc_name}.variable.time_series.png'
     
 
-def make_combined_files(verbose=False, make_plot=False, save_plot=False):
+def make_combined_files(outdir='.', verbose=False, make_plot=False, save_plot=False):
     '''Merges station files'''
     recipes = get_recipe()
 
     for recipe in recipes:
-        if verbose: print(f'Combining files for {recipe['location']}')
+        if verbose: print(f'Combining files for {recipe["location"]}')
         combined_df = combine_files(recipe)
         #print_record_timerange(combined_df)
         #print_variable_flags(combined_df)
