1d5904cc6f3905a7da789fb120c40c2c6ff16b65
Andy Barrett <apbarret@nsidc.org>
2022-02-04 16:49:50 -0700

    add columns to write, add eol

--- canadian_extreme_precip/write_files.py
+++ canadian_extreme_precip/write_files.py
@@ -1,5 +1,23 @@
 '''Module for writing station data to stdout of file'''
 
+these_columns = [
+    'MEAN_TEMPERATURE',
+    'MEAN_TEMPERATURE_FLAG',
+    'MIN_TEMPERATURE',
+    'MIN_TEMPERATURE_FLAG',
+    'MAX_TEMPERATURE',
+    'MAX_TEMPERATURE_FLAG',
+    'TOTAL_PRECIPITATION',
+    'TOTAL_PRECIPITATION_FLAG',
+    'TOTAL_RAIN',
+    'TOTAL_RAIN_FLAG',
+    'TOTAL_SNOW',
+    'TOTAL_SNOW_FLAG',
+    'SNOW_ON_GROUND',
+    'SNOW_ON_GROUND_FLAG',
+    ]
+
+
 def record_fmt(rec):
     '''Generates a formated string for a data record
 
@@ -14,10 +32,16 @@ def record_fmt(rec):
     varnames = [v for v in data.index if '_FLAG' not in v]
     for v in varnames:
         sfmt.append(var_fmt(data, v))
-    return ' '.join(sfmt)
+    return ' '.join(sfmt) + '\n'
 
 
 def var_fmt(r, var):
     var_flag = var + '_FLAG'
     return f'{r[var]:6.1f} {r[var_flag]:3}' 
 
+
+def write_formatted_data(df, outfile, columns=these_columns):
+    with open(outfile, 'w') as f:
+        for record in df[these_columns].iterrows():
+            f.write(record_fmt(record))
+    return
