6bdf2cefeb1c92a54292f262aa924e3523f44462
Andy Barrett <apbarret@nsidc.org>
2022-02-07 16:06:54 -0700

    add path to combined files

--- canadian_extreme_precip/filepath.py
+++ canadian_extreme_precip/filepath.py
@@ -7,6 +7,7 @@ HOME = Path.home()
 DATAPATH = HOME / 'Data' / 'Rain_on_snow'
 
 RAW_STATION_PATH = DATAPATH / 'Canada_extreme_precip' / 'Raw_station_files'
+COMBINED_PATH = DATAPATH / 'Canada_extreme_precip' / 'Combined_files'
 
 
 def raw_station_filepath(climate_identifier):
