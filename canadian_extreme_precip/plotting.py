9d84153a4491993f3a8f213216efd33d294a4b22
Andy Barrett <apbarret@nsidc.org>
2022-02-07 21:28:29 -0700

    fix ticks

--- canadian_extreme_precip/plotting.py
+++ canadian_extreme_precip/plotting.py
@@ -1,5 +1,6 @@
 '''Plotting routines for Extreme Precip Paper'''
 import datetime as dt
+import numpy as np
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -17,13 +18,16 @@ def station_heatmap(df, ax=None, cmap=None):
         ax = plt.gca()
 
     z = df.values.T
+    z = np.where(z > 0., z, np.nan)
+    
     x = mdates.date2num(df.index)
     y = df.columns
-    
+    #z = np.broadcast_to(np.arange(len(y)), (z.shape[1],z.shape[0])).T / len(y)
+        
     # Get extent of heatmap array
     x0 = df.index[0]
     xlim = mdates.date2num([dt.datetime(x0.year, x0.month, 1), df.index[-1]])
-    ylim = [-0.5, len(y)-0.5]
+    ylim = [len(y)-0.5, -0.5]
 
     img = ax.imshow(z, aspect='auto', cmap=cmap,
                     extent=[xlim[0], xlim[-1], ylim[0], ylim[1]])
@@ -34,8 +38,12 @@ def station_heatmap(df, ax=None, cmap=None):
     ax.set_xticks(xticks)
     date_format = mdates.DateFormatter('%Y')
     ax.xaxis.set_major_formatter(date_format)
+
+    ax.set_yticks(np.arange(7))
+    ax.set_yticklabels(y)
     
-    #ax.set_yticklabels(y)
+    for yy in (np.arange(len(y)) - 0.5)[1:]:
+        ax.axhline(yy, color='k')
     
     plt.colorbar(img, ax=ax)
 
