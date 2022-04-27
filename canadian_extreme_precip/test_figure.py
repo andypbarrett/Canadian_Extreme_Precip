04537c1226b454604e8b33796bc12a02428fbbba
Andy Barrett <apbarret@nsidc.org>
2022-04-26 18:30:14 -0600

    add proof of concept

--- canadian_extreme_precip/test_figure.py
+++ canadian_extreme_precip/test_figure.py
@@ -1,30 +1,50 @@
 """Test for plotting subaxes"""
 
+import numpy as np
+
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 
+def dummy_plot1(ax):
+    x = np.arange(1, 13)
+    y = np.sin((x-1) * np.pi / 11) + np.random.randn(12)
+    ax.plot(x, y)
+    ax.axhline(0.)
+    ax.set_ylabel('Temperature', fontsize=15)
+    return ax
+    
+    
+def dummy_plot2(ax):
+    x = np.arange(1, 13)
+    y = np.sin((x-1) * np.pi / 11) + np.random.randn(12)
+    ax.fill_between(x, y, step='mid')
+    ax.axhline(0.)
+    ax.set_ylabel('Cyclones', fontsize=15)
+    return ax
+    
+    
 def climatology_panel(gs):
     """Generate a climatology panel"""
-    subgs = gs.subgridspec(4, 1, hspace=0.)
+    fig = plt.gcf()
+    subgs = gridspec.GridSpecFromSubplotSpec(4, 1, hspace=0., subplot_spec=gs)
     ax1 = fig.add_subplot(subgs[:-1, :])
+    dummy_plot1(ax1)
     ax2 = fig.add_subplot(subgs[-1, :])
+    dummy_plot2(ax2)
     return subgs
 
 
 def main():
     fig = plt.figure(figsize=(15, 20))
 
-    gs0 = gridspec.GridSpec(1, 2, figure=fig)
+    gs0 = gridspec.GridSpec(3, 3, figure=fig)
 
-    gs00 = gs0[0].subgridspec(4, 1, hspace=0.)
-    ax1 = fig.add_subplot(gs00[:-1, :])
-    ax2 = fig.add_subplot(gs00[-1, :])
-
-    gs01 = gs0[1].subgridspec(4, 1, hspace=0.)
-    ax3 = fig.add_subplot(gs01[:-1, :])
-    ax4 = fig.add_subplot(gs01[-1, :])
-
+    gs_sub = []
+    for gs in gs0:
+        gs_sub.append(climatology_panel(gs))
+    plt.tight_layout()
+    
     plt.show()
     
 
