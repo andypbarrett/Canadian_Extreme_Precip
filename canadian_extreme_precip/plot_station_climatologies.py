2cbe7ba8c1c24d29f6fb3a797a1060292a4edc45
Andy Barrett <apbarret@nsidc.org>
2022-05-06 16:26:05 -0600

    refactor to load climatology file using reader

--- canadian_extreme_precip/plot_station_climatologies.py
+++ canadian_extreme_precip/plot_station_climatologies.py
@@ -4,9 +4,8 @@ for Canadian Extreme Precip paper"""
 import matplotlib.pyplot as plt
 
 from plotting import plot_climatology
-from reader import read_combined_file
-from filepath import FIGURE_PATH, combined_station_filepath
-from utils import to_monthly, to_climatology
+from reader import read_climatology
+from filepath import FIGURE_PATH, climatology_filepath
 
 
 # Excludes Pond Inlet
@@ -23,15 +22,6 @@ stations_list = [
     ]
 
 
-def load_climatology(station):
-    """Loads station data and calculates climatology
-    and loads cyclone frequency"""
-    df = read_combined_file(combined_station_filepath(station))
-    df_mon = to_monthly(df)
-    df_clm = to_climatology(df_mon.loc['1960':'1995', :])
-    return df_clm
-
-    
 def plot_station_climatologies(verbose=False):
     """Main function for plotting climatologies"""
 
@@ -39,7 +29,7 @@ def plot_station_climatologies(verbose=False):
 
     for ax, station in zip(axes.flatten(), stations_list):
         if verbose: print(f"Reading data for {station.title()}")
-        df = load_climatology(station)
+        df = read_climatology(climatology_filepath(station))
         plot_climatology(df, ax=ax, title=station)
 
     fig.tight_layout()
@@ -47,5 +37,5 @@ def plot_station_climatologies(verbose=False):
 
 
 if __name__ == "__main__":
-    verbose=True
+    verbose = True
     plot_station_climatologies(verbose=verbose)
