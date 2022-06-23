cf72cc0a3cabede6faa60d42bb952a9501872a01
Andy Barrett <apbarret@nsidc.org>
2022-06-23 16:08:54 -0600

    initial commit

---
+++ canadian_extreme_precip/plot_era5_thickness_climatology.py
@@ -0,0 +1,54 @@
+"""Plots T2m and TCWV July climatology for paper"""
+
+from pathlib import Path
+
+import numpy as np
+import xarray as xr
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import ListedColormap
+
+import cartopy.crs as ccrs
+
+from plotting import plot_panarctic_panel
+
+datafile = Path('/projects/AROSS/Reanalysis/ERA5/pressure_levels/monthly/',
+                'era5.pressure_levels.monthly.climatology.1980to2010.nc')
+
+cbar_kwargs = {'shrink': 0.95, 'orientation': 'horizontal', 'pad': 0.05}
+
+plt.rcParams.update({'mathtext.default':  'regular' })
+
+thk_cmap = cm.get_cmap('cividis', 256)
+thk_cmap = ListedColormap(thk_cmap(np.linspace(0.0, 0.9, 256)))
+
+
+def plot_era5_thickness_climatology():
+    """Plots climatology figure"""
+
+    ds = xr.open_dataset(datafile)
+
+    fig = plt.figure(figsize=(7,7))
+
+    ax1 = plot_panarctic_panel(fig, 111)
+    cbar_kwargs['label'] = 'm'
+    ds.thickness.sel(month=7).plot.contourf(ax=ax1,
+                                            transform=ccrs.PlateCarree(),
+                                            vmin=5200, vmax=5600,
+                                            levels=np.arange(5280., 5600.+40., 40.),
+                                            cmap=thk_cmap,
+                                            cbar_kwargs=cbar_kwargs,)
+    ax1.set_title('')
+    ax1.text(0.02, 0.98, 'a) $\Delta z$',
+             transform=ax1.transAxes,
+             fontsize=17,
+             horizontalalignment='left',
+             verticalalignment='top',
+             bbox={'facecolor': 'white', 'alpha': 0.5,})
+    
+    fig.savefig('era5.thickness.climatology.png')
+
+
+if __name__ == "__main__":
+    plot_era5_thickness_climatology()
