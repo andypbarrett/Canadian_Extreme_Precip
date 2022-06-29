2440df360a7c02d2120f295f3c25e353e2587422
Andy Barrett <apbarret@nsidc.org>
2022-06-29 01:00:16 -0600

    add path for figures

--- canadian_extreme_precip/plot_station_location_map.py
+++ canadian_extreme_precip/plot_station_location_map.py
@@ -1,15 +1,18 @@
 """Plots location of stations"""
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 
 from plotting import location_map
+
+FIGURE_PATH = Path.home() / 'src' / 'Canadian_extreme_precip' / 'figures'
 
 
 def plot_station_location_map(figsize=(10, 7)):
     """Plots station location map"""
     fig = plt.figure(figsize=figsize)
     ax = location_map(fig)
-    fig.savefig('station_location_map.png')
+    fig.savefig(FIGURE_PATH / 'station_location_map.png')
 
 
 if __name__ == "__main__":
