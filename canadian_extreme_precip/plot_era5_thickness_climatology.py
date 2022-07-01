0b145359689fd1025e92467e5f334d1a0686b9c8
Andy Barrett <apbarret@nsidc.org>
2022-06-30 18:59:24 -0600

    change contour levels, mask greenland

--- canadian_extreme_precip/plot_era5_thickness_climatology.py
+++ canadian_extreme_precip/plot_era5_thickness_climatology.py
@@ -11,7 +11,8 @@ from matplotlib.colors import ListedColormap
 
 import cartopy.crs as ccrs
 
-from plotting import plot_panarctic_panel
+from plotting import plot_panarctic_panel, mask_greenland
+
 
 datafile = Path('/projects/AROSS/Reanalysis/ERA5/pressure_levels/monthly/',
                 'era5.pressure_levels.monthly.climatology.1980to2010.nc')
@@ -21,7 +22,7 @@ cbar_kwargs = {'shrink': 0.95, 'orientation': 'horizontal', 'pad': 0.05}
 plt.rcParams.update({'mathtext.default':  'regular' })
 
 thk_cmap = cm.get_cmap('cividis', 256)
-thk_cmap = ListedColormap(thk_cmap(np.linspace(0.0, 0.9, 256)))
+thk_cmap = ListedColormap(thk_cmap(np.linspace(0.1, 1.0, 256)))
 
 
 def plot_era5_thickness_climatology():
@@ -36,7 +37,7 @@ def plot_era5_thickness_climatology():
     ds.thickness.sel(month=7).plot.contourf(ax=ax1,
                                             transform=ccrs.PlateCarree(),
                                             vmin=5200, vmax=5600,
-                                            levels=np.arange(5280., 5600.+40., 40.),
+                                            levels=np.arange(5400., 5600.+20., 20.),
                                             cmap=thk_cmap,
                                             cbar_kwargs=cbar_kwargs,)
     ax1.set_title('')
@@ -46,6 +47,7 @@ def plot_era5_thickness_climatology():
              horizontalalignment='left',
              verticalalignment='top',
              bbox={'facecolor': 'white', 'alpha': 0.5,})
+    mask_greenland(ax1)
     
     fig.savefig('era5.thickness.climatology.png')
 
